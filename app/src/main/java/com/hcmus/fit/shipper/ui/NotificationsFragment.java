3cf7f8ba05fdf039c68e63a4ebf7e53c97268c07
hoanganhgo <anhluong660@gmail.com>
2021-04-25 23:01:26 +0700

    tabs bar

---
+++ app/src/main/java/com/hcmus/fit/shipper/ui/NotificationsFragment.java
@@ -0,0 +1,25 @@
+package com.hcmus.fit.shipper.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.hcmus.fit.shipper.R;
+
+public class NotificationsFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+        final TextView textView = root.findViewById(R.id.text_notifications);
+        textView.setText("Notifications");
+
+        return root;
+    }
+}
